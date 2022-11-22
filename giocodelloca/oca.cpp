b7259f141baa8869a018253065601bfcc1161288
socket772 <paolotto.ghilardi@gmail.com>
2022-11-22 15:20:33 +0100

    Update oca and oca.cpp

--- giocodelloca/oca.cpp
+++ giocodelloca/oca.cpp
@@ -18,8 +18,16 @@ int tiroDado()
     return rand()%11+2;
 }
 
-int movimento(int posGiocatore)
+int movimento(int posGiocatore, int &turniCasa)
 {
+
+    //casa
+    if (posGiocatore==19)
+    {
+        turniCasa--;
+        return posGiocatore;
+    }
+
     int tiro = tiroDado();
     posGiocatore=posGiocatore+tiro;
 
@@ -31,21 +39,35 @@ int movimento(int posGiocatore)
     if (posGiocatore==42)
         posGiocatore=39;
     
-    //labirinto
+    //scheletro
     if (posGiocatore==58)
         posGiocatore=1;
 
-    return 0;
+    return posGiocatore;
 }
 
 int main()
 {
     int nGiocatori=2; //numero giocatori effettivi-1
     int posP1, posP2=0;
+    int casaP1, casaP2=0;
     int turnoP=1; //indica di chi è il turno, 0=P0, 1=P1,...
     //cout<<avanzaTurno(turnoP, nGiocatori)<<endl;
 
+    posP1 = 19;
+    
+    
+    while (true)
+    {
+        posP1 = movimento(posP1, casaP1);
+        avanzaTurno(turnoP, nGiocatori);
+        cout << posP1 << " : " << casaP1 << endl;
 
+        if(posP1==63 || posP2==63)
+            break;
+    }
+    
+    
 
     return 0;
 }
